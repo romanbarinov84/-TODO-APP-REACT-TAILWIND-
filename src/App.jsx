b5b82fb9d8ee3152ee5ce0c9c674fd3b6f82c394
Roman Meinl <roman-meinl@ukr.net>
2025-12-15 11:46:12 -0200

    15.12 onDelete function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,76 @@
+import { useState } from "react";
+import TodoItem from "../src/components/TodoItem"
 
 import "./App.css";
 
-
 function App() {
-  
+  const initialTodos = [
+    { id: 1, text: "Сделать кофе" },
+    { id: 2, text: "Одеться" },
+    { id: 3, text: "Пойти на работу" },
+  ];
 
-  
+  const [todos, setTodos] = useState(initialTodos);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (!text.trim()) return;
+
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        text,
+      },
+    ]);
+
+    setText("");
+  };
+
+  const onDelete = (id) => {
+     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
+  }
+
   return (
-    <>
-    </>
-  )
-  
+    <div className="min-h-screen bg-gray-100 flex justify-center pt-10">
+      <div className="w-full max-w-md bg-white rounded-2xl p-6 shadow">
+        <h1 className="text-2xl font-bold mb-6 text-center text-indigo-500">
+          React Todo-App on Tailwind
+        </h1>
+
+        <div className="flex gap-3 mb-6">
+          <input
+            type="text"
+            placeholder="Введите текст"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && addTodo()}
+            className="flex-1 rounded-2xl border border-gray-300 px-4 py-2
+                       focus:border-indigo-500 focus:ring-2 focus:ring-indigo-500/30
+                       outline-none transition"
+          />
+
+          <button
+            onClick={addTodo}
+            className="rounded-2xl bg-indigo-600 px-4 py-2
+                       text-white font-semibold
+                       hover:bg-indigo-700
+                       active:scale-95
+                       focus:ring-2 focus:ring-indigo-500/40
+                       transition"
+          >
+            ➕
+          </button>
+        </div>
+
+        <ul className="space-y-2">
+          {todos.map((todo) => (
+           <TodoItem key={todo.id} todo={todo} onDelete={onDelete}/>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,27 @@
-const TodoItem = () => {
+const TodoItem = ({ todo , onDelete }) => {
   return (
-    <>
-    
-    </>
+    <div
+      key={todo.id}
+      className="flex justify-between rounded-2xl border-2 border-gray-300 px-4 py-2 
+                       hover:border-indigo-500 hover:bg-pink-700 hover:text-white focus:ring-2 focus:ring-indigo-500/30
+                       outline-none transition"
+    >
+      <span>{todo.text}</span>
+      <div className="flex gap-2">
+        <button className="rounded-2xl bg-green-300 px-2 py-1
+                       text-white font-semibold
+                       hover:bg-green-700
+                       active:scale-95
+                       focus:ring-2 focus:ring-indigo-500/40
+                       transition">Сделано</button>
+        <button className="rounded-2xl bg-red-300 px-2 py-1
+                       text-white font-semibold
+                       hover:bg-red-700
+                       active:scale-95
+                       focus:ring-2 focus:ring-indigo-500/40
+                       transition" onClick={() => onDelete(todo.id)}>Удалить</button>
+      </div>
+    </div>
   );
 };
 
